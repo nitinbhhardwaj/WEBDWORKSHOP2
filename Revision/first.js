5cdf337dac7e54eb88cefa81d524f45124895456
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-03-03 14:55:52 +0530

    Created another file and used it with node.js

--- Revision/first.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello Nitin");
-console.log("Welcome to CSE 21");
-console.log("This is my first Javascript");
---
+++ Revision/new.js
@@ -0,0 +1,3 @@
+  
+        console.log("Hello guys");
+        console.log("I know how to do this");
