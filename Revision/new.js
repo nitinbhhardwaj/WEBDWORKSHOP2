9bc927c286429c48c932763e2554d9c5ae7629dd
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-03-05 21:56:39 +0530

    Creating basic function

--- Revision/new.js
+++ Revision/new.js
@@ -1,4 +1,11 @@
-  let x=56;
-  console.log(x);
-        console.log("Hello guys");
-        console.log("I know how to do this");
+//   var x;
+//   x=56;
+//   console.log(x);
+//         console.log("Hello guys");
+//         console.log("I know how to do this");
+//         console.log(x);
+function solve() {
+    var age=25;
+    console.log(age);
+}
+solve();
