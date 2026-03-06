2e844712fc46ca618840518e0bd6fe7676ae829e
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-03-06 10:56:33 +0530

    Using const variable

--- Revision/new.js
+++ Revision/new.js
@@ -12,3 +12,7 @@
 // solve();
 
 //Javascript is a dynamically typed language because we can use different type of data in the same variable. We can assign a number to a variable and then we can assign a string to the same variable unlike the statically typed langauges like c++, java.
+
+{const name;
+ name="Myanak";
+console.log(name);}
