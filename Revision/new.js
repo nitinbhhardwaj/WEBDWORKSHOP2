67e054c62172dada043529a69b1a6c8f4dc1dea3
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-03-06 10:50:05 +0530

    comment added regarding javascript being a dynamically typed language

--- Revision/new.js
+++ Revision/new.js
@@ -4,9 +4,11 @@
 //         console.log("Hello guys");
 //         console.log("I know how to do this");
 //         console.log(x);
-function solve() {
-    var age=25;
-    console.log(age);
-}
-console.log(age);
-solve();
+// function solve() {
+//     var age=25;
+//     console.log(age);
+// }
+// console.log(age);
+// solve();
+
+//Javascript is a dynamically typed language because we can use different type of data in the same variable. We can assign a number to a variable and then we can assign a string to the same variable unlike the statically typed langauges like c++, java.
