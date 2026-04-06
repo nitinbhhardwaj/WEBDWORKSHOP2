17fddaaf1c0a681772b93ef077883549d928e415
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-06 10:09:55 +0530

    Add conditional statements examples in conditionals.js

---
+++ Revision/JavaS/conditionals.js
@@ -0,0 +1,24 @@
+// age=24;
+// if(age>=18) console.log("adult");
+// else console.log('minor');
+
+// num=22;
+// if(num%2==0) console.log("even number");
+// else console.log("odd");
+
+/*Ternary operator*/
+// n=5;
+// (n%2==0)? console.log("even"): console.log("odd");
+
+/*Rating system*/
+// rating=1;
+// if(rating==5) console.log("Excellent");
+// else if(rating ==4) console.log("Good");
+// else if(rating==3) console.log("Average");
+// else if(rating==2)console.log("Below Average");
+// else console.log("Poor");
+
+/*********Three digit number**********/
+num=899.1;
+if(num>=100 && num<=999) console.log("Three digit number");
+else console.log("Not a three digit number");
