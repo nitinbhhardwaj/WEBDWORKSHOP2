9b803f57297e881f9d99f943a61baf6ab41bc3ee
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-06 09:53:34 +0530

    Add data types demonstration in JavaScript

---
+++ Revision/JavaS/dataTypes.js
@@ -0,0 +1,14 @@
+a=2;
+b=3.15;
+c="Mayank";
+d=true;
+e=null;
+f='q';
+console.log(a,typeof (a));
+console.log(b,typeof (b));
+console.log(c,typeof (c));
+console.log(d,typeof (d));
+console.log(e,typeof (e));
+console.log(f,typeof (f));
+
+
--- Revision/datatypes.js
+++
@@ -1,2 +0,0 @@
-let marks;
-console.log(marks);
