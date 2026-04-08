4c35c65b5288f87d0b86650fbf56a840fc318816
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-09 05:05:08 +0530

    Add mathematical functions for absolute, max, min, power, logarithm, square root, cube root, floor, ceil, and random number generation

---
+++ Revision/JavaS/mathematicalFunctions.js
@@ -0,0 +1,16 @@
+console.log(Math.abs(-7));
+
+console.log(Math.max(5,4,3,2,1));
+console.log(Math.min(5,3,2,75));
+
+console.log(Math.pow(2,4));
+console.log(Math.log10(10000));
+
+console.log(Math.sqrt(64));
+console.log(Math.cbrt(64));
+
+console.log(Math.floor(3.5));
+console.log(Math.ceil(3.4));
+
+console.log(Math.random(0,9));
+
