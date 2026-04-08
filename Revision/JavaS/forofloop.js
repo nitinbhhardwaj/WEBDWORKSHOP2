795545fb2740d92231f6188d06be93b22b39f537
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-08 21:48:18 +0530

    Trying different cases for forof loop

--- Revision/JavaS/forofloop.js
+++ Revision/JavaS/forofloop.js
@@ -1,12 +1,17 @@
 arr=[3,4,5,2,6];
-brr=[4,6,3,6,2];
-for(let i=0;i<arr.length;i++){
-    console.log(2*arr[i]);
-}
-for (const ele of arr){
-    console.log(2*ele);
-}
+object=[4,6,3,6,2];
+// for(let i=0;i<arr.length;i++){
+//     console.log(2*arr[i]);
+// }
+// for (const ele of arr){
+//     console.log(2*ele);
+// }
 
+//forof loop
+for (const iterator of object){
+    // object[iterator]*=2;
+    console.log(2*iterator);
+}
 
 
 
