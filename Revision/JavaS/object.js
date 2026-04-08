61de7af91ab5d694db2390385001165e8f578991
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-09 04:43:12 +0530

    Add object definition and example usage for key-value pairs

---
+++ Revision/JavaS/object.js
@@ -0,0 +1,24 @@
+//Object is king of like map/dictionary where we have key-value pairs
+var details= ["Raghav",24,92.5,false];
+
+let obj={
+    name: "Nitin",
+age: 24,
+percentage:92.5,
+"is Married":false
+};
+// console.log(obj.age,obj.name,obj["is Married"]);
+// obj.age=40;
+// console.log(obj["age"]);
+// obj["age"]=50;
+// console.log(obj["age"]);
+
+// for(const key in obj){
+//     if(Object.hasOwnProperty.call(obj,key)){
+//         console.log(key,obj[key]);
+//     }
+// }
+
+// for(const key of Object.keys(obj)){
+//     console.log(key,obj[key]);
+// }
