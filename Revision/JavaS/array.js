e9d9ba6eaf65e68a7108a6b6eed14cd4c7e3db17
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-08 17:06:49 +0530

    creating array using const variable type

--- Revision/JavaS/array.js
+++ Revision/JavaS/array.js
@@ -9,10 +9,16 @@ arr=[1,2,4,5,6];
 
 //console.log(arr[3]);
 
-arr.push(9);
+arr.push(9); //add from last
 console.log(arr);
+
 arr.pop(); //remove the last element of the array
 console.log(arr);
-arr.unshift(23);
+
+arr.unshift(23);//add from the beginning
 console.log(arr);
+
+arr.shift();//remove the first element
+console.log(arr);
+
 //C++ : Vectors, Java : ArrayList 
---
+++ Revision/JavaS/constarray.js
@@ -0,0 +1,9 @@
+let array=[2,3,4,5,6];
+array[0]=1;
+console.log(array);
+array=5;
+console.log(array);
+
+const arr=[2,3,5,6,6];
+arr[0]=1;
+console.log(arr);
