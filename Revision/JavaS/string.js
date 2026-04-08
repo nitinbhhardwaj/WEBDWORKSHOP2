d8af2d352dab4a82c5ff8ef7ae3fb43d12e5c2f9
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-09 02:21:52 +0530

    slice,trim,toLowerCase and toUpperCase and many such properties are used

--- Revision/JavaS/string.js
+++ Revision/JavaS/string.js
@@ -1,5 +1,7 @@
-let s= "Coder";
-console.log(s,s.length);
+let s= "  CoderC  is a platform";
+
+// console.log(s,s.length);
+
 // for(let i=0;i<s.length;i++){
 //     console.log(s[i]);
 // }
@@ -12,3 +14,19 @@ console.log(s,s.length);
 //for each loop does not work on string 
 
 console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.trim());
+console.log(s.length);
+console.log(s.indexOf ('C'));
+console.log(s.lastIndexOf('C'));
+console.log(s.charAt(3));
+console.log(s[3]);
+console.log(s.slice(5)); //substring from index 5 onwards
+console.log(s.slice(2,7));//substring from index 2 to 6
+
+let arr=s.split(' ');
+console.log(arr);
+for(const ele of arr){
+    console.log(ele);
+}
+
