e341bd85389ea5def99230c9ba1f697ce6282c1b
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-08 22:32:36 +0530

    Using builtin functions of strings

---
+++ Revision/JavaS/string.js
@@ -0,0 +1,14 @@
+let s= "Coder";
+console.log(s,s.length);
+// for(let i=0;i<s.length;i++){
+//     console.log(s[i]);
+// }
+
+
+// for(const char of s){
+//     console.log(char);
+// }
+
+//for each loop does not work on string 
+
+console.log(s.toUpperCase());
