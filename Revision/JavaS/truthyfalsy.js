311e510bca208963d396eb1170c0b0b1e5a427d8
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-08 22:09:11 +0530

    Analysed truthy and falsy values

---
+++ Revision/JavaS/truthyfalsy.js
@@ -0,0 +1,9 @@
+//False values -> false condition, false , 0, null ,undefined ,NaN
+//Truthy values -> everythign except false values
+
+if(1){
+    console.log("This is true"); //truthy
+}
+else console.log("This is false"); //falsy
+
+console.log(typeof a);
