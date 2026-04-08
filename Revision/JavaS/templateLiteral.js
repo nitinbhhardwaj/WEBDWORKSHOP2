5dde0348eab4206a58ebbaf84fa83d9c6b1e14d0
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-09 04:21:43 +0530

    Add template literal examples for age and experience logging

---
+++ Revision/JavaS/templateLiteral.js
@@ -0,0 +1,6 @@
+let age=30;
+let exp=10;
+console.log("My age is"+ age+ " and experience is" +exp+ "years");
+console.log(`My age is ${age}`);
+
+console.log(`My age is ${age} and experience is ${exp} years`);
