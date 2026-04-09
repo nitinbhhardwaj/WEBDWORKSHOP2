1c495528655929931d3e5cf0bceb49fab0152270
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-09 06:14:28 +0530

    Add random number generation examples for ranges 0-9, 0-10, and 0-100

---
+++ Revision/JavaS/ques1.js
@@ -0,0 +1,9 @@
+//Write a code to generate a random number between 0 and 9 (included)
+let num=Math.random()*10;
+console.log(Math.floor(num));
+
+//Write a code to generate a random number between 0 and 10 (included)
+console.log(Math.floor(Math.random()*11));
+
+//Write a code to generate a random number between 0 and 100 (included)
+console.log(Math.floor(Math.random()*101));
