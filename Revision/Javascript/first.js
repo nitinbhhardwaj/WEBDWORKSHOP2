4870d8f26423d55ae6ac68a4ddfb002b24f2443d
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-04-16 21:36:36 +0530

    feat: add initial HTML structure and JavaScript file for learning purposes

---
+++ Revision/Javascript/first.js
@@ -0,0 +1,23 @@
+console.log("Hello world");
+console.log("Welcome to JS");
+console.log("Thisis not my first JS code");
+age=24.2;
+console.log(age);
+namee="Tony Stark";
+console.log(typeof age);
+x=null;
+y=undefined;
+console.log(typeof x);
+console.log(typeof y);
+radius=3;
+istrue=true;
+console.log(typeof istrue);
+console.log("Javascript is a dynamically typed language");
+
+//Javascript is a csase sensitive language
+fullname="Rahul";
+fullName="Mayank";
+console.log(fullname);
+console.log(fullName);
+//Space and first num is not allowed in variable name (letter,digit,_,$)
+
