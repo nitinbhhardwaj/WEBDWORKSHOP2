8fec5365f7e662c895827a00cc5d5712a159e474
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-07 19:55:00 +0530

    Add type checks and console logs for variable 'a' in helloWorld.js

--- Revision/JavaScr/helloWorld.js
+++ Revision/JavaScr/helloWorld.js
@@ -3,4 +3,10 @@ let x="nitin";
 console.log(typeof(x));
 console.log(543+1);
 console.log("3");
+a=4;
+console.log(typeof(a));
+console.log(a+10);
+console.log(x,"is Perfect");
+a="Nitin";
+console.log(typeof(a));
 
