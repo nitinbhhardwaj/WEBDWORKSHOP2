ce20d87f29ce4d94bd70b8befc94d129a17af538
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-07 22:09:36 +0530

    loops in JS

--- Revision/JavaScr/conditionals.js
+++ Revision/JavaScr/conditionals.js
@@ -7,10 +7,39 @@
 //     console.log("Odd");
 // }
 
-a=14;
-if(a>=18){
-    console.log("Eligible for voting");
-}
-else{
-    console.log("Not eligible to vote");
-}
+// a=14;
+// if(a>=18){
+//     console.log("Eligible for voting");
+// }
+// else{
+//     console.log("Not eligible to vote");
+// }
+
+// rating=6;
+// if(rating==1){
+//     console.log("Poor");
+// }
+// else if(rating==2){
+//     console.log("Below Average");
+// }
+// else if(rating==3){
+//     console.log("Average");
+// }
+// else if(rating==4){
+//     console.log("Good");
+// }
+// else if(rating==5){
+//     console.log("Excellent");
+// }
+// else{
+//     console.log("Rate b/w 1 to 5");
+// }
+
+// x=452;
+// if(x>99 && x<1000){
+//     console.log("3 digit number");
+// }
+// else{
+//     console.log("Not a 3 digit number");
+// }
+
---
+++ Revision/JavaScr/tripleEqual.js
@@ -0,0 +1,15 @@
+a=3;
+b="3";
+// if(a==b){
+//     console.log("Equal");
+// }
+ 
+// if(a===b){
+//     console.log("Equal");
+// }
+// else{
+//     console.log("This is a triple equal operator");
+// }
+
+console.log(a===b);
+console.log(a==b);
