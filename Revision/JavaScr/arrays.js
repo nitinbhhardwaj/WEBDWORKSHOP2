56923600c217031c44027a283f858e12635c5061
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-08 10:12:50 +0530

    Created a sample code for Bootstrap

---
+++ Revision/JavaScr/arrays.js
@@ -0,0 +1,16 @@
+x=[2,3,4,5];
+console.log(x);
+
+// y=x[2];
+// console.log(y);
+
+// for(let i=0;i<x.length;i++){
+//     console.log(x[i]);
+// }
+
+x.push(6);
+console.log(x);
+
+x.pop();
+console.log(x);
+
---
+++ Revision/JavaScr/loops.js
@@ -0,0 +1,24 @@
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<=10);
+
+for(let i=1;i<=10;i++){
+    let a="";
+    for(let j=1;j<=10-i;j++){
+        a += "*";
+    }
+    console.log(a);
+}
