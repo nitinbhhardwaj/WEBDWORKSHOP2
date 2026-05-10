7b86c1c5ce80fdc04051bbdf2f775bd75a9bf3a8
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-10 13:26:10 +0530

    Another function for sum of two numbers

--- Revision/JavaScr/functions.js
+++ Revision/JavaScr/functions.js
@@ -1,7 +1,12 @@
-function OnetoN(n){
-    for(let i=1;i<=n;i++){
-        console.log(i);
-    }
-}
+// function OnetoN(n){
+//     for(let i=1;i<=n;i++){
+//         console.log(i);
+//     }
+// }
 
-OnetoN(20);
+// OnetoN(20);
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(2,3);
