edb2b2501d20c71bf40c8822b9518a9972e2b269
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-10 19:01:22 +0530

    Used builtin function (Math.abs) to get the absolute value of the number

--- Revision/JavaScr/functions.js
+++ Revision/JavaScr/functions.js
@@ -6,7 +6,19 @@
 
 // OnetoN(20);
 
-function sum(a,b){
-    console.log((a*a)+(b*b));
+// function sum(a,b){
+//     console.log((a*a)+(b*b));
+// }
+// sum(2,3);
+
+// function sum(a,b){
+//     a=Math.abs(a);
+//     b=Math.abs(b);
+//     console.log(a*a*a+b*b*b);
+// }
+// sum(2,-3);
+
+function cube(a,b){
+     return ((Math.abs(a*a*a))+(Math.abs(b*b*b)));
 }
-sum(2,3);
+console.log(cube(2,-3));
