f67a20d4e9ae3bb91c39fb6fe7afcbbc4719a2a4
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-12 18:35:44 +0530

    add initial setTimeout.js file with commented-out examples

---
+++ Revision/JavaScr/setTimeOut.js
@@ -0,0 +1,16 @@
+
+// for(let i=30;i>=1;i--){
+//    setTimeout(function(){
+//     console.log(i)},Math.abs((i-30)*200)
+//    );
+//}
+
+
+// setTimeout(function(){for(let i=1;i<=10;i++){
+//     console.log(i);
+// }},2000);
+
+
+
+
+
