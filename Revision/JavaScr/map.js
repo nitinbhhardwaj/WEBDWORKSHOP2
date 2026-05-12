aae01a3d9bf456fa45fe58d039e8476ef3f70343
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-12 19:59:27 +0530

    created a separate file for map function for better understanding

--- Revision/JavaScr/map.js
+++ Revision/JavaScr/map.js
@@ -1,13 +1,33 @@
-function square(x){
-    return x*x;
-}
+// function square(x){
+//     return x*x;
+// }
 
-function add(x){
-    return x+10;
-}
-let arr=[2,3,4,5,6];
-let brr=arr.map(square);
+// function add(x){
+//     return x+10;
+// }
+// let arr=[2,3,4,5,6];
+// let brr=arr.map(square);
+// console.log(brr);
+
+// let crr=arr.map(add);
+// console.log(crr);
+
+// let drr=arr.map(function(x){
+//     return x-2;
+// });
+// console.log(drr);
+
+// let err=arr.map((x)=>{
+//     return x*x*x;
+// });
+// console.log(err);
+
+// let frr=arr.map(x=>x/2);
+// console.log(frr);
+
+
+let arr=[1,2,3,4,5];
+let brr=arr.map(
+    (ele)=> 2*ele
+);
 console.log(brr);
-
-let crr=arr.map(add);
-console.log(crr);
