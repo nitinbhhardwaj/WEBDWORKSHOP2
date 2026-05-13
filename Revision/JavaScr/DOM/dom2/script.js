72966fdc2bde3f51b6196d6ecfa1990f778c793e
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-14 04:14:35 +0530

    created a basic project code using Event Listener

--- Revision/JavaScr/DOM/dom2/script.js
+++ Revision/JavaScr/DOM/dom2/script.js
@@ -29,19 +29,22 @@ y.addEventListener("mouseleave",function(){
 })
 let z=document.getElementById("box3");
 z.addEventListener("mousemove",function(){
-    let r=Math.floor(Math.random*255);
-    let g=Math.floor(Math.random*255);
-    let b=Math.floor(Math.random*255);
-    z.style.backgroundColor=rgb(r,g,b);
+    let r=Math.floor(Math.random()*256);
+    let g=Math.floor(Math.random()*256);
+    let b=Math.floor(Math.random()*256);
+    z.style.backgroundColor=`rgb(${r},${g},${b})`;
 })
 z.addEventListener("mouseleave",function(){
     z.style.backgroundColor=rgb(255,255,255);
 })
 let a=document.getElementById("box4");
 a.addEventListener("mousemove",function(){
-    x.style.backgroundColor="blue";
-    y.style.backgroundColor="navy";
-    z.style.backgroundColor="skyblue";
+    let r=Math.floor(Math.random()*256);
+    let g=Math.floor(Math.random()*256);
+    let b=Math.floor(Math.random()*256);
+    x.style.backgroundColor=`rgb(${r},255,${b})`;
+    y.style.backgroundColor=`rgb(255,${g},${b})`;
+    z.style.backgroundColor=`rgb(${r},${g},255)`;
 })
 a.addEventListener("mouseleave",function(){
     x.style.backgroundColor="white";
