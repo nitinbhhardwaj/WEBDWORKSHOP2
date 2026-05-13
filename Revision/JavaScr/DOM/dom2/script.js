8d95f8f18f88083fb29a1c76817d79da120bcf83
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-13 21:51:14 +0530

    Added event listeners for the another box where the alternate colors are added

--- Revision/JavaScr/DOM/dom2/script.js
+++ Revision/JavaScr/DOM/dom2/script.js
@@ -1,20 +1,37 @@
 let x=document.getElementById("box1");
-x.addEventListener("click",function(){
-    x.innerText=Math.round(Math.random(0,10));
+x.addEventListener("mouseenter",function(){
+    let r=Math.floor(Math.random()*100);
+    x.innerText=`${r}`;
 })
+x.addEventListener("mouseleave",function(){
+    x.innerText=1;
+})
+
+
 let y=document.getElementById("box2");
-y.addEventListener("click",function(){
-    y.style.backgroundColor="Red";
+let clr="green";
+y.addEventListener("mouseenter",function(){
+    if(clr=="green"){
+        y.style.backgroundColor="green";
+        clr="red";
+    }
+    else{
+        y.style.backgroundColor="red";
+        clr="green";
+    }
 })
 y.addEventListener("mouseleave",function(){
     y.style.backgroundColor="white";
 })
 let z=document.getElementById("box3");
 z.addEventListener("mousemove",function(){
-    let r=Math.random*256;
-    let g=Math.random*256;
-    let b=Math.random*256;
-
+    let r=Math.random*255;
+    let g=Math.random*255;
+    let b=Math.random*255;
+    z.style.backgroundColor=rgb(255,255,255);
+})
+z.addEventListener("mouseleave",function(){
+    z.style.backgroundColor=rgb(0,0,0);
 })
 let a=document.getElementById("box4");
 a.addEventListener("mousemove",function(){
