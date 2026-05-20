82df31a2beccc597a79f81c0653525c0e674f38c
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-20 18:48:40 +0530

    making changes in App.jsx to get the desired output

---
+++ Cards/src/Cards.jsx
@@ -0,0 +1,10 @@
+function Cards(){
+    return(
+        <div>
+            <img alt="hello"></img>
+            <h2>Bro Code</h2>
+            <p>I make Youtube videos and play video games</p>
+        </div>
+    );
+}
+export default Cards
