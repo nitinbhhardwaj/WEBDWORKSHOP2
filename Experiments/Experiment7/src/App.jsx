f4a3d78adc497c2086c99fbf783045f1dde91411
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-20 19:38:29 +0530

    Basics of React studied

--- Experiments/Experiment7/src/App.jsx
+++ Experiments/Experiment7/src/App.jsx
@@ -1,12 +1,12 @@
-import Student from "./Lab/Experiment 7/Student";
+import Student from "./Experiments/Experiment 7/Student";
 function App() {
 return (
 <div className="app">
 <h1>Student Details</h1>
 <div className="student-list">
-<Student name="Faraz Farooqui" course="B.Tech AIML" marks={85} />
-<Student name="Bipul Patel" course="B.Tech AIML" marks={90} />
-<Student name="Aman Shukla" course="B.Tech AIML" marks={97} />
+<Student name="Nitin Bhardwaj" course="B.Tech CSE" marks={99} />
+<Student name="Mayank Kumar" course="B.Tech AIML" marks={90} />
+<Student name="Ojas Chaudhary" course="B.Tech DS" marks={97} />
 </div>
 </div>
 );
