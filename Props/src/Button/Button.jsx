d45154aff9c4754ca810bc816ceac2b988146aff
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-20 21:18:08 +0530

    Used modules correctly

--- Props/src/Button/Button.jsx
+++ Props/src/Button/Button.jsx
@@ -1,7 +1,7 @@
-
+import styles from './Button.module.css'
 function Button(){
     return(
-        <button >Click me</button>
+        <button className={styles.button}>Click me</button>
     );
 }
 export default Button
