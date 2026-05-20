d3e9b23376bb914f1a9f2da80581e1167900e26e
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-20 16:14:34 +0530

    deleted a sample-project which was added for revision

---
+++ Revision/JavaScr/DOM/dom3/script.js
@@ -0,0 +1,9 @@
+// alert("Javascript is used in the file");
+// console.dir(document.body);
+// document.body.childNodes[1].innerText="abcd";
+let x=document.getElementById("header");
+console.log(x);
+let y=document.getElementsByTagName("h2");
+console.log(y);
+y[0].style.backgroundColor="green";
+
