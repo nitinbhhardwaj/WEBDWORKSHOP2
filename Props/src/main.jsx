e69771559d9ab54b78ea862afc0f25e43f39b825
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-20 22:16:16 +0530

    Got to know that prop types are not used anymore

--- Props/src/main.jsx
+++ Props/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+//props= read-only properties that are shared between components.
+//       A parent component can send data to a child component.
+//       <Component key=valye/>
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
