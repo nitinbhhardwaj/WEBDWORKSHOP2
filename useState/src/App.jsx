01f1d2b28832581f3efad94678180661f12ce369
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-05-21 06:46:02 +0530

    Created a basic program of useState

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -2,9 +2,12 @@
 //             to use React features without writing class components (React v126.8)
 //             (useState,useEffect,useContext,useReducer,useCallback, and more...)
 
-import MyComponent from "./MyComponent"
+import MyComponent from "./MyComponent.jsx"
 function App() {
   return (
+    <>
+    <MyComponent/>
+    </>
     
   );
 }
--- useState/src/MyComponent.jsx
+++ useState/src/MyComponent.jsx
@@ -1,4 +1,28 @@
+import React, {useState} from 'react';
 function MyComponent(){
-    return();
+    const [name,setName]=useState("Guest");
+    const updateName=()=>{
+        setName("Spongebob");
+    }
+
+    const[age,setAge]=useState(0);
+    const incrementAge=()=>{
+        setAge(age+1);
+    }
+
+    const[isEmployed,setToggle]=useState(false);
+    const toggleEmployedStatus=()=>{
+        setToggle(!isEmployed);
+    }
+    return(<div>
+        <p>Name: {name}</p>
+                <button onClick={updateName}>Set Name</button>
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>Increment Age</button>
+
+        <p>Is employed:  {(isEmployed)? "Yes": "No"}</p>
+        <button onClick={toggleEmployedStatus}>Set EmployedStatus</button>
+        
+    </div>);
 }
 export default MyComponent
